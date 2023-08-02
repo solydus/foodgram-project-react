319d03a4fc7dc90ac4f71d225e61b5568bf45eb0
solydus <igoshevs@gmail.com>
2023-08-02 11:51:55 +0300

    flake8 all project

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -71,22 +71,17 @@ DATABASES = {
 
 DEFAULT_AUTO_FIEL = 'django.db.models.BigAutoField'
 
-
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME':
+     'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'
+     },
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'
+     },
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'
+     },
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'
+     },
 ]
-
 
 LANGUAGE_CODE = 'ru'
 
--- backend/users/migrations/0001_initial.py
+++ backend/users/migrations/0001_initial.py
@@ -8,6 +8,7 @@ import django.db.models.expressions
 import django.utils.timezone
 from django.conf import settings
 from django.db import migrations, models
+from django.contrib.auth.validators import UnicodeUsernameValidator
 
 
 class Migration(migrations.Migration):
@@ -36,17 +37,17 @@ class Migration(migrations.Migration):
                 ('is_superuser',
                  models.BooleanField(
                      default=False,
-                     help_text='Designates that this user has all permissions without explicitly assigning them.',
+                     help_text='Designates that this user has all permissions',
                      verbose_name='superuser status')),
                 ('is_staff',
                  models.BooleanField(
                      default=False,
-                     help_text='Designates whether the user can log into this admin site.',
+                     help_text='Designates whether the this admin site.',
                      verbose_name='staff status')),
                 ('is_active',
                  models.BooleanField(
                      default=True,
-                     help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.',
+                     help_text='Designates whether',
                      verbose_name='active')),
                 ('date_joined',
                  models.DateTimeField(
@@ -54,12 +55,12 @@ class Migration(migrations.Migration):
                      verbose_name='date joined')),
                 ('username',
                  models.CharField(
-                     max_length=150,
-                     unique=True,
-                     validators=[
-                         django.contrib.auth.validators.UnicodeUsernameValidator,
-                         api.validators.validate_username],
-                     verbose_name='Логин')),
+                    max_length=150,
+                    unique=True,
+                    validators=[
+                        UnicodeUsernameValidator,
+                        api.validators.validate_username],
+                    ),)
                 ('first_name',
                  models.CharField(
                      max_length=150,
@@ -84,7 +85,7 @@ class Migration(migrations.Migration):
                 ('groups',
                  models.ManyToManyField(
                      blank=True,
-                     help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+                     help_text='The groups this user belongs to. A user will',
                      related_name='user_set',
                      related_query_name='user',
                      to='auth.Group',
