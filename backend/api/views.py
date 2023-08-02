3ce7b12f322a392eef95d7bf75a80521568c7583
solydus <97594065+solydus@users.noreply.github.com>
2023-08-02 20:50:07 +0300

    Update views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,7 @@
 from django.db.models import Sum
 from django.shortcuts import HttpResponse, get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import status, viewsets
+from rest_framework import status, viewsets, mixins
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
