de2c4bb7095685bf473fec08b6936e411a831c0e
solydus <97594065+solydus@users.noreply.github.com>
2023-08-02 22:37:44 +0300

    Update mixins.py

--- backend/api/mixins.py
+++ backend/api/mixins.py
@@ -1,7 +1,7 @@
 from rest_framework import mixins, viewsets
 
 
-class CreateDestroyViewSet(mixins.CreateModelMixin,
+class CreateDestroyAll(mixins.CreateModelMixin,
                            mixins.DestroyModelMixin,
                            viewsets.GenericViewSet):
     """
